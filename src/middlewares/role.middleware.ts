526a3b923c83f540e69fb2552e037568b2335e9f
JuuKabyar <177juu17@gmail.com>
2026-06-23 14:08:24 +0630

    Add role middleware

--- src/middlewares/role.middleware.ts
+++ src/middlewares/role.middleware.ts
@@ -1,1 +1,22 @@
 // ckeck roles
+
+import { Request, Response, NextFunction } from "express";
+import { errorResponse } from "../utils/response";
+
+export const roleMiddleware = (roles: string[]) => {
+
+  return (req: Request, res: Response, next: NextFunction) => {
+
+    const userRole = req.user?.role; // Get user role from token
+
+    if(!userRole){
+      return errorResponse(res, 401, "User Role Not Found.")
+    }
+
+    if(!roles.includes(userRole)){
+      return errorResponse(res, 403, "Access Denied.")
+    }
+
+    next(); // Continue to next controller
+  }
+}
